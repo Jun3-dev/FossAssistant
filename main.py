4346477c836339afc757bbd0a06a14ca2cc3d105
Jun Lee <82641992+Jun3-dev@users.noreply.github.com>
2021-08-22 11:46:53 +1000

    Update main.py

--- main.py
+++ main.py
@@ -4,50 +4,64 @@ import pywhatkit
 import datetime
 import wikipedia
 import pyjokes
+import python_weather
+import asyncio
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
-voices = engine.getProperty("voices")
-engine.setProperty("voice", voices[1].id)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
 
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
+
 def take_command():
     try:
         with sr.Microphone() as source:
-            print("Listening...")
+            print('listening...')
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
-            if "uno" in command:
-                command = command.replace("uno", "")
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
                 print(command)
     except:
         pass
-    return command
+    return
 
 
-def run_alexa():
+def run_fossistant():
     command = take_command()
     print(command)
-    if "play" in command:
-        song = command.replace("play", "")
-        talk("Playing" + song)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
         pywhatkit.playonyt(song)
-    elif "time" in command:
-        time = datetime.datetime.now().strftime("%H:%M %p")
-        print(time)
-        talk("The time is" + time)
-    elif "wikipedia" in command:
-        person = command.replace("wikipedia", "")
-        info = wikipedia.summary(person,3)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%H:%M %p')
+        talk('Current time is ' + time)
+    elif 'wikipedia' in command:
+        person = command.replace('wikipedia', '')
+        info = wikipedia.summary(person, 3)
         print(info)
         talk(info)
-    elif "joke" in command:
+    elif 'joke' in command:
         talk(pyjokes.get_joke())
+    elif "search" in command:
+        search = command.replace("Uno", "")
+        search = command.replace("search", "")
+        pywhatkit.search(search)
+        talk("Searching" + search)
+        print("Searching" + search)
+    elif "weather" in command:
+        pass
+    else:
+        talk('Please say the command again.')
+
 
 
 while True:
-    run_alexa()
+    run_fossistant()
